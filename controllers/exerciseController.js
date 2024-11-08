ecff27b06f9fc8d6b5ae70521d87237ac4f61a84
Taras Chuiko <tchuiko@griddynamics.com>
2024-11-08 10:37:07 +0100

    Added error handling

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -3,55 +3,70 @@ const { User } = require("../models/User");
 const { v4: uuidv4 } = require("uuid");
 
 const createExercise = async (req, res) => {
-  const defaultDate = new Date().toISOString().split("T")[0];
+  try {
+    const user = await User.findById(req.params._id);
 
-  const newExercise = new Exercise({
-    _id: uuidv4(),
-    userId: req.params._id,
-    description: req.body.description,
-    duration: req.body.duration,
-    date: req.body.date || defaultDate,
-  });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
 
-  await newExercise.save();
+    const defaultDate = new Date().toISOString().split("T")[0];
 
-  const user = await User.findById(req.params._id);
+    const newExercise = new Exercise({
+      _id: uuidv4(),
+      userId: req.params._id,
+      description: req.body.description,
+      duration: req.body.duration,
+      date: req.body.date || defaultDate,
+    });
 
-  res.json({
-    username: user.username,
-    description: newExercise.description,
-    duration: newExercise.duration,
-    date: newExercise.date,
-    _id: user._id,
-  });
+    await newExercise.save();
+
+    res.json({
+      username: user.username,
+      description: newExercise.description,
+      duration: newExercise.duration,
+      date: new Date(newExercise.date).toDateString(),
+      _id: user._id,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Could not create exercise" });
+  }
 };
 
 const getLogs = async (req, res) => {
-  const user = await User.findById(req.params._id);
+  try {
+    const user = await User.findById(req.params._id);
 
-  const { from, to, limit } = req.query;
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
 
-  const dateFilter = {};
-  if (from) dateFilter.$gte = new Date(from);
-  if (to) dateFilter.$lte = new Date(to);
+    const { from, to, limit } = req.query;
 
-  const exercises = await Exercise.find({
-    userId: req.params._id,
-    ...(Object.keys(dateFilter).length > 0 && { date: dateFilter }),
-  })
-    .limit(parseInt(limit) || 0)
-    .exec();
+    const dateFilter = {};
+    if (from) dateFilter.$gte = new Date(from);
+    if (to) dateFilter.$lte = new Date(to);
 
-  res.json({
-    username: user.username,
-    count: exercises.length,
-    _id: user._id,
-    log: exercises.map((exercise) => ({
-      description: exercise.description,
-      duration: exercise.duration,
-      date: new Date(exercise.date).toDateString(),
-    })),
-  });
+    const exercises = await Exercise.find({
+      userId: req.params._id,
+      ...(Object.keys(dateFilter).length > 0 && { date: dateFilter }),
+    })
+      .limit(parseInt(limit) || 0)
+      .exec();
+
+    res.json({
+      username: user.username,
+      count: exercises.length,
+      log: exercises.map((exercise) => ({
+        description: exercise.description,
+        duration: exercise.duration,
+        date: new Date(exercise.date).toDateString(),
+      })),
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Could not get logs" });
+  }
 };
 
 module.exports = {
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,20 +2,28 @@ const { User } = require("../models/User");
 const { v4: uuidv4 } = require("uuid");
 
 const createUser = async (req, res) => {
-  const newUser = new User({
-    _id: uuidv4(),
-    username: req.body.username,
-  });
+  try {
+    const newUser = new User({
+      _id: uuidv4(),
+      username: req.body.username,
+    });
 
-  await newUser.save();
+    await newUser.save();
 
-  res.json(newUser);
+    res.json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: "Could not create user" });
+  }
 };
 
 const getUsers = async (req, res) => {
-  const users = await User.find();
+  try {
+    const users = await User.find();
 
-  res.json(users);
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Could not get users" });
+  }
 };
 
 module.exports = {
